559677b57494b1d4a01a0acc8dd817e4f067f2d0
nishant0207 <dalalnishant0207@gmail.com>
2024-01-28 05:56:50 +0530

    added signup page
    added signup page and transition functionality between login and signup page

--- frontend/src/Screens/LoginPage.jsx
+++ frontend/src/Screens/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
@@ -7,6 +7,32 @@ import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import styled from "styled-components";
+
+const SignUpContainer = styled.div`
+  position: absolute;
+  top: 0;
+  transition: all 0.6s ease-in-out;
+  left: 0;
+  height:100%;
+  opacity: 1;
+  width: 50%;
+  z-index: 2;
+  ${props =>
+    props.signingIn !== true
+      ? `transform: translateY(-100%); opacity: 0;`: null}
+`;
+
+const SignInContainer = styled.div`
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+  left: 0;
+  width: 50%;
+  z-index: 2;
+  ${props => (props.signingIn !== true ? `transform: translateY(100%); opacity:0` : null)}
+`;
 
 
 const Login = () => {
@@ -100,6 +126,12 @@ const Login = () => {
             });
     }
 
+    const [isLoginFormVisible, setIsLoginFormVisible] = useState(true);
+
+    const handleToggleForms = () => {
+        setIsLoginFormVisible(!isLoginFormVisible);
+    };
+
     return (
         // below div is the outer most container which have whole page inside
         <div style={{
@@ -129,45 +161,110 @@ const Login = () => {
                     <p style={{ fontFamily: "monospace", fontSize: "30px", fontWeight: "bold", color: "white" }}>Discover Your Ideal Haven: Where Hostel Life Meets Comfort, Community, and Convenience.</p>
                 </div>
 
-                {/* below is the login form and sign up form area */}
                 <div style={{
-                    height: "65vh",
-                    width: "30vw",
-                    border: "1px solid lightgray",
-                    borderRadius: "20px",
-                    boxShadow: "10px 10px 8px rgba(0,0,0,0.2)",
-                    display: 'flex',
-                    flexDirection: "column",
-                    padding: "30px",
-                    margin: "auto auto",
-                    background: "white"
-                }}>
-                    <h1>Login</h1>
+                        overflow:"hidden", 
+                        height:"70vh", 
+                        width: "30vw",
+                        borderRadius: "20px",
+                        margin: "auto auto",
+                        background: "white",
+                        position:'relative',
+                        boxShadow: "10px 10px 8px rgba(0,0,0,0.2)",
+                        }}>
+                    {/* below is the login form and sign up form area */}
+                    {/* <Container> */}
+                    <SignUpContainer signingIn={!isLoginFormVisible}>
+                    <div style={{
+                        height: "70vh",
+                        width: "30vw",
+                        border: "1px solid lightgray",
+                        borderRadius: "20px",
+                        boxShadow: "10px 10px 8px rgba(0,0,0,0.2)",
+                        display: 'flex',
+                        flexDirection: "column",
+                        padding: "30px",
+                        margin: "auto auto",
+                        background: "white",
+                        position:"absolute",
+                        transition: "all 0.6s ease-in-out",
+                    }}
+                        id="login"
+                        className="form-wrapper sign-in"
+                    >
+                        <h1>Login</h1>
 
-                    <p>Hi, Welcome back</p>
+                        <p>Hi, Welcome back</p>
 
-                    <button style={{ width: "100%", alignSelf: "center" }} class="btn btn-outline-success" type="button" onClick={handleLoginWithGoogle}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-google" viewBox="0 0 16 16">
-                            <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
-                        </svg>  Login with Google</button>
+                        <button style={{ width: "100%", alignSelf: "center" }} class="btn btn-outline-success" type="button" onClick={handleLoginWithGoogle}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-google" viewBox="0 0 16 16">
+                                <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
+                            </svg>  Login with Google</button>
 
-                    <p style={{ fontFamily: "monospace", color: "gray", fontSize: 15, alignSelf: "center", marginTop: "20px" }}>--------- or Login with Email ---------</p>
+                        <p style={{ fontFamily: "monospace", color: "gray", fontSize: 15, alignSelf: "center", marginTop: "20px" }}>--------- or Login with Email ---------</p>
 
-                    <form action="" style={{ width: "100%" }}>
-                        <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Email</text> <br />
+                        <form action="" style={{ width: "100%" }}>
+                            <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Email</text> <br />
 
-                        <input type="email" id="emailid" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Email" required />
-                        <br /> <br />
+                            <input type="email" id="emailid" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Email" required />
+                            <br /> <br />
 
-                        <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Password</text> <br />
+                            <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Password</text> <br />
 
-                        <input type="password" id="password" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Password" required />
-                        <br /> <br />
+                            <input type="password" id="password" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Password" required />
+                            <br /> <br />
 
-                        <button style={{ width: "100%", alignSelf: "center" }} type="button" id="Login" class="btn btn-outline-primary" onClick={handleLogin}>Login</button>
+                            <button style={{ width: "100%", alignSelf: "center" }} type="button" id="Login" class="btn btn-outline-primary" onClick={handleLogin}>Login</button>
 
-                        <p>Don't have an account? <span style={{ color: "rgb(49,108,244)" }}>Create an account</span></p>
-                    </form>
+                            <p style={{ marginTop: "20px" }}>Don't have an account? <span style={{ color: "rgb(49,108,244)", cursor: "pointer" }} id="signInBtn-Link" onClick={handleToggleForms}>Create an account</span></p>
+                        </form>
+                    </div>
+                    </SignUpContainer>
+
+                    <SignInContainer signingIn={isLoginFormVisible}>
+                    <div style={{
+                        height: "70vh",
+                        width: "30vw",
+                        border: "1px solid lightgray",
+                        borderRadius: "20px",
+                        boxShadow: "10px 10px 8px rgba(0,0,0,0.2)",
+                        display: 'flex',
+                        flexDirection: "column",
+                        padding: "30px",
+                        margin: "auto auto",
+                        background: "white"
+                    }}
+                        id="signup"
+                        className="form-wrapper sign-up"
+                    >
+                        <h1>Sign Up</h1>
+
+                        <p>Hi, Welcome back</p>
+
+                        <button style={{ width: "100%", alignSelf: "center" }} class="btn btn-outline-success" type="button" onClick={handleLoginWithGoogle}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-google" viewBox="0 0 16 16">
+                                <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z" />
+                            </svg>  Login with Google</button>
+
+                        <p style={{ fontFamily: "monospace", color: "gray", fontSize: 15, alignSelf: "center", marginTop: "20px" }}>--------- or SignUp with Email ---------</p>
+
+                        <form action="" style={{ width: "100%" }}>
+                            <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Email</text> <br />
+
+                            <input type="email" id="emailid" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Email" required />
+                            <br /> <br />
+
+                            <text style={{ fontFamily: "monospace", fontSize: "20px" }}>Password</text> <br />
+
+                            <input type="password" id="password" style={{ border: "1px solid lightgray", width: "100%", height: 40, borderRadius: "5px" }} placeholder="Password" required />
+                            <br /> <br />
+
+                            <button style={{ width: "100%", alignSelf: "center" }} type="button" id="Login" class="btn btn-outline-primary" onClick={handleSignup}>Signup</button>
+
+                            <p style={{ marginTop: "20px" }} id="signUpBtn-Link">Already have an account? <span style={{ color: "rgb(49,108,244)", cursor: "pointer" }} onClick={handleToggleForms}>Login</span></p>
+                        </form>
+                    </div>
+                    </SignInContainer>
+                    {/* </Container> */}
                 </div>
             </div>
 
